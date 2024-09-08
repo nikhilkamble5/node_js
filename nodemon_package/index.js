7f01d70b74dcbf3a53615f51ed15d45fd0974a73
nikhilkamble5 <nikhilkamble19985@gmail.com>
2024-09-09 00:48:56 +0530

    simple api

--- nodemon_package/index.js
+++ nodemon_package/index.js
@@ -1,1 +1,6 @@
-console.log("try Nodemon");
+console.log("try nodemon new");
+console.log(100+20);
+
+
+// q node js i async or sync ans=> async
+
---
+++ simple_api/data.js
@@ -0,0 +1,7 @@
+const data=[
+    {name:'Nik',email:'nik@test'},
+    {name:'rik',email:'wik@test'},
+    {name:'sik',email:'sik@test'},
+    {name:'pik',email:'cik@test'},
+]
+module.exports=data;
